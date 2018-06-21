ab494afed25b7f2744df32b7ba75193e73b6c220
wufengw <wufengw@hcc-1340-13.vancouver.sfu.ca>
2018-06-21 14:57:24 -0700

    Initial Commit

---
+++ I AM SUPER DANK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I AM SUPER DANK
+//
+//  Created by wufengw on 6/21/18.
+//  Copyright © 2018 Kaybo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
